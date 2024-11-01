8c560f676290c43148e01962e5941019377cbbcc
Rafael Lacerda Silva <167188437+Taff4@users.noreply.github.com>
2024-11-01 18:43:20 -0300

    Update vendas.h

--- vendas.h
+++ vendas.h
@@ -1,6 +1,7 @@
 #ifndef VENDAS_H
 #define VENDAS_H
 
+// Estrutura que define os atributos de uma Venda
 typedef struct {
     int codigo_produto;
     float peso;
@@ -8,7 +9,10 @@ typedef struct {
     char data[11];
 } Venda;
 
+// Função para registrar uma venda
 void registrarVenda();
+
+// Função para gerar o relatório de vendas
 void gerarRelatorio();
 
 #endif
